895b763b6ced9ff81891e5e2471fb2a18d5e77ea
Gavin Lam <gavin.oss@tutamail.com>
2024-12-06 22:01:37 -0500

    Print solution running time when running tests

--- tests/aoc2024/test_solutions.py
+++ tests/aoc2024/test_solutions.py
@@ -1,5 +1,6 @@
 """Run solutions and compare against the answers."""
 
+import timeit
 import unittest
 
 from aoclibs import executions, files
@@ -8,8 +9,15 @@ from aoclibs import executions, files
 class TestSolutions(unittest.TestCase):
     """Run solutions and compare against the answers."""
 
+    # pylint: disable=cell-var-from-loop
     def test_solutions(self):
         """Run solutions with the defined input parser and result printer."""
         for star in range(1, 13):
             answer = files.data_file_content(2024, "star", star, path_prefix="/tests")
-            self.assertEqual(answer, executions.run_solution(2024, star))
+            time = timeit.timeit(
+                lambda: self.assertEqual(answer, executions.run_solution(2024, star)),
+                number=1,
+            )
+            ms = round(time * 1000, 2)
+
+            print(f"Solution for star {star} took {ms}ms to finish")
