8910010017e49b54d5f8fbbd3f3d99406f9dfaa7
Gavin Lam <gavin.oss@tutamail.com>
2024-12-12 11:47:04 -0500

    Completed 2024 day 12 puzzles

---
+++ aoc2024/solutions/star23.py
@@ -0,0 +1,53 @@
+"""Solution for 2024 star 23.
+
+Problem page:
+    https://adventofcode.com/2024/day/12
+
+Solutions:
+    1. DFS
+        - O(mn) time, O(mn) auxiliary space
+"""
+
+from aoclibs import inputs
+
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
+def run(grid: list[list[str]]) -> int:
+    """Calculate the total cost of fences."""
+    rows, cols = len(grid), len(grid[0])
+    visited = [[False] * cols for _ in range(rows)]
+    costs = 0
+
+    def visit_area(plant: str, row: int, col: int) -> tuple[int, int]:
+        area, perimeter = 1, 0
+
+        for dr, dc in DIRECTIONS:
+            nr, nc = row + dr, col + dc
+            if not (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == plant):
+                perimeter += 1
+                continue
+            if visited[nr][nc]:
+                continue
+
+            visited[nr][nc] = True
+            n_area, n_perimeter = visit_area(plant, nr, nc)
+            area, perimeter = area + n_area, perimeter + n_perimeter
+
+        return (area, perimeter)
+
+    for row in range(rows):
+        for col in range(cols):
+            if visited[row][col]:
+                continue
+
+            visited[row][col] = True
+            area, perimeter = visit_area(grid[row][col], row, col)
+            costs += area * perimeter
+
+    return costs
+
+
+PARSER = inputs.parse_char_grid
+PRINTER = str
---
+++ aoc2024/solutions/star24.py
@@ -0,0 +1,70 @@
+"""Solution for 2024 star 24.
+
+Problem page:
+    https://adventofcode.com/2024/day/12#part2
+
+Solutions:
+    1. DFS + bitmasking
+        - O(mn) time, O(mn) auxiliary space
+"""
+
+from aoclibs import inputs
+
+
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+UNVISITED = -1
+
+
+def run(grid: list[list[str]]) -> int:
+    """Calculate the total cost of fences."""
+    rows, cols = len(grid), len(grid[0])
+    costs = 0
+
+    visited = [[UNVISITED] * cols for _ in range(rows)]
+    borders = [[0] * cols for _ in range(rows)]
+
+    def visit_area(plant: str, row: int, col: int, step: int) -> tuple[int, int]:
+        area, sides = 1, 0
+
+        # Find all borders
+        for direction, (dr, dc) in enumerate(DIRECTIONS):
+            nr, nc = row + dr, col + dc
+            if not (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == plant):
+                sides += 1
+                borders[row][col] |= 1 << direction
+
+        # Visit neighbors
+        for dr, dc in DIRECTIONS:
+            nr, nc = row + dr, col + dc
+            if not (0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == plant):
+                continue
+
+            if visited[nr][nc] != UNVISITED:
+                # Only deduct from cells being visited earlier to avoid double counting
+                if step < visited[nr][nc]:
+                    sides -= (borders[row][col] & borders[nr][nc]).bit_count()
+                continue
+
+            visited[nr][nc] = step + 1
+            n_area, n_sides = visit_area(plant, nr, nc, step + 1)
+            area += n_area
+
+            # Adjust for continuous borders
+            sides += n_sides - (borders[row][col] & borders[nr][nc]).bit_count()
+
+        return (area, sides)
+
+    for row in range(rows):
+        for col in range(cols):
+            if visited[row][col] != UNVISITED:
+                continue
+
+            visited[row][col] = 0
+            area, sides = visit_area(grid[row][col], row, col, 0)
+            costs += area * sides
+
+    return costs
+
+
+PARSER = inputs.parse_char_grid
+PRINTER = str
