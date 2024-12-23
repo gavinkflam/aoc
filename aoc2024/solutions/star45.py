61b841a6736cf4cbd776d0cc8df1be80233c6da7
Gavin Lam <gavin.oss@tutamail.com>
2024-12-23 12:48:16 -0500

    Completed 2024 day 23 part 2

--- aoc2024/solutions/star45.py
+++ aoc2024/solutions/star45.py
@@ -4,11 +4,13 @@ Problem page:
     https://adventofcode.com/2024/day/23
 
 Solutions:
-    1. Brute force
-        - O(n * p^2 + m) time, O(n + m) auxiliary space
+    1. Backtracking
+        - O(nC3 + m) time, O(n + m) auxiliary space
             where n = number of computers,
-                  m = number of pairs,
-                  p = maximum number of neighbors of a computer
+                  m = number of pairs
+    2. Better brute force
+        - O(n * p^2 + m) time, O(n + m) auxiliary space
+            where p = maximum number of neighbors of a computer
 """
 
 from collections import defaultdict
@@ -16,21 +18,29 @@ from collections import defaultdict
 from aoclibs import inputs
 
 
+AdjacencyList = dict[str, set[str]]
+
+
 def split_pairs(lines: list[str]) -> list[tuple[str, str]]:
     """Split each line into a pair of computer."""
     return [tuple(line.split("-")) for line in lines]
 
 
-def run(lines: list[str]) -> int:
-    """Find the number of three-computer groups with a member's name starting with t."""
-    pairs = split_pairs(lines)
-
-    # Construct adjacency list
+def build_adjacency_list(pairs: list[tuple[str, str]]) -> AdjacencyList:
+    """Build a adjacency list from the given pairs."""
     adj_list = defaultdict(set)
 
     for u, v in pairs:
         adj_list[u].add(v)
         adj_list[v].add(u)
+
+    return adj_list
+
+
+def run(lines: list[str]) -> int:
+    """Find the number of three-computer groups with a member's name starting with t."""
+    pairs = split_pairs(lines)
+    adj_list = build_adjacency_list(pairs)
 
     # Find groups satisfying the requirements
     good_groups = 0
--- aoc2024/solutions/star46.py
+++ aoc2024/solutions/star46.py
@@ -4,15 +4,72 @@ Problem page:
     https://adventofcode.com/2024/day/23#part2
 
 Solutions:
-    1. Todo
+    1. Backtracking
+        - O(2^n * n^2) time, O(n + m) auxiliary space
+            where n = number of computers,
+                  m = number of pairs
+    2. Better backtracking
+        - O(n * 2^p * p^2) time, O(n + m) auxiliary space
+            where p = maximum number of neighbors of a computer
+    3. Growing groups
+        - O(m * p^k * klogk) time, O(n + m) auxiliary space
+            where k = number of computers in the largest interconnected group
+
+Further research:
+    This is the problem of finding the maximum clique, which is a NP-complete problem.
 """
 
+from collections import namedtuple
+
+from aoc2024.solutions import star45
+from aoc2024.solutions.star45 import AdjacencyList
 from aoclibs import inputs
 
 
-def run(_: list[str]) -> str:
+Group = namedtuple("Group", ["leader", "members"])
+
+
+def build_group(adj_list: AdjacencyList, *members: str) -> tuple[str, Group]:
+    """Build a group and designate the member with the least neighbors as the leader."""
+    leader = None
+    signature = ",".join(sorted(members))
+
+    for member in members:
+        if not leader or len(adj_list[member]) < len(adj_list[leader]):
+            leader = member
+
+    return (signature, Group(leader, set(members)))
+
+
+def run(lines: list[str]) -> str:
     """Find the members of the largest interconnected group of computers."""
-    return "todo"
+    pairs = star45.split_pairs(lines)
+    adj_list = star45.build_adjacency_list(pairs)
+    n = len(adj_list)
+
+    # Find the largest group by growing the current largest groups by one member
+    groups = dict(build_group(adj_list, *pair) for pair in pairs)
+
+    for _ in range(3, n + 1):
+        new_groups = {}
+
+        for leader, members in groups.values():
+            for candidate in adj_list[leader]:
+                if candidate in members:
+                    continue
+
+                # A new member needs to be connected to all members of the group
+                if all(candidate in adj_list[member] for member in members):
+                    signature, new_group = build_group(adj_list, *members, candidate)
+                    if signature not in new_groups:
+                        new_groups[signature] = new_group
+
+        # No larger groups can be found
+        if not new_groups:
+            break
+        groups = new_groups
+
+    return next(iter(groups))
 
 
 PARSER = inputs.parse_str_lines
