1afd1539a0f2eb1977a30a21bfe2e851c05d9bb0
Gavin Lam <gavin.oss@tutamail.com>
2025-12-03 00:14:54 -0500

    Completed 2025 day 3 part 1

---
+++ aoc2025/solutions/star05.py
@@ -0,0 +1,41 @@
+"""Solution for 2025 star 5.
+
+Problem page:
+    https://adventofcode.com/2025/day/3
+
+Solutions:
+    1. Brute force
+        - O(m * n^2) time, O(1) auxiliary space,
+            where m = number of lines,
+                  n = length of each line
+    2. Greedy
+        - O(mn) time, O(1) auxiliary space
+"""
+
+from aoclibs import inputs
+
+
+def run(lines: list[str]) -> int:
+    """Find the sum of the maximum joltage possible from each bank."""
+    banks = [[int(ch) for ch in line] for line in lines]
+    ans = 0
+
+    for bank in banks:
+        n = len(bank)
+        fst, snd = bank[0], bank[1]
+
+        for i in range(2, n):
+            battery = bank[i]
+
+            if snd > fst:
+                fst, snd = snd, battery
+            elif battery >= snd:
+                fst, snd = max(fst, snd), battery
+
+        ans += fst * 10 + snd
+
+    return ans
+
+
+PARSER = inputs.parse_str_lines
+PRINTER = str
